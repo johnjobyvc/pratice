0a9f43b1c25c8fce04ae756f1917aa68e6fff737
john <john.joby@anotete.co.jp>
2026-06-02 12:01:22 +0900

    zxzxc

--- index.php
+++ index.php
@@ -1,31 +1,5 @@
 <?php echo "Hello, World!";
-echo "This is the index.php file.";
-echo phpinfo ();
-sdfasdf
-sdf
-asdf
-asd
-f
-sad
-f
-print "test";
-#############################
-@@@@@@@@@@@@@@@@@@@@@@@@
-TESTNEW#########################
-#################
-MAIN TWO BRANCH;
-echo "NEWW COMM";
-sdafasf
-sad
-fas
-df
-asdf
-echo "18 EXAMPLE OF REBASE";
-<<<PRE>>> TEST TEST TEST  <<</PRE>>>
-NEW ADDED 6/6/2024S
-echo 'FORCE PUSH';
-NEW LINE AEED 7:07 PM;
-sdafasdfsadf
-Date 6/2/2026
-<<<pre>>>  SECOND PRE<<</pre>>>
+TTTTTTTTTTTTTTTT
+STASCH THISffffffffffffffffffffffffffffffff
+TESTNEWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW;
 ?>
